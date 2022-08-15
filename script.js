757246b31af3bc8ced83878d7aa736c8848db753
Crishawna <crishawna.nash@gmail.com>
2022-08-15 14:18:24 -0500

    finished

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-// Write your JavaScript code here!
+// // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
 
+    // when the submit button is clicked it will go to script helper to validate all info given and then update the form based off of incorrect or correct input
+    document.getElementById('formSubmit').addEventListener('click', function(event){
+        event.preventDefault();
+        formSubmission(document, 
+            document.getElementById('faultyItems'), 
+            document.getElementById('pilotName').value, 
+            document.getElementsByName('copilotName')[0].value, 
+            document.getElementsByName('fuelLevel')[0].value, 
+            document.getElementsByName('cargoMass')[0].value);
+    }) 
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        const pick = pickPlanet(listedPlanets);
+        // console.log(pick);
+        addDestinationInfo(document, pick.name, pick.diameter, pick.star, pick.distance, pick.moons, pick.image)
    })
    
 });
